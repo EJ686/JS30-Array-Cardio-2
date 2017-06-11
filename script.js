8a88a7aecb0ca3de0d11cd25960dfe71e41aaa05
Edgar Juarez <ejjt.686@gmail.com>
2017-06-11 16:56:08 -0700

    Used array find prototype to find an unique comment id

--- script.js
+++ script.js
@@ -34,6 +34,10 @@ console.log({allAdult});
 // Find is like filter, but instead returns just the one you are looking for
 // find the comment with the ID of 823423
 
+const comment = comments.find(comments => comments.id === 823423);
+
+console.log(comment);
+
 // Array.prototype.findIndex()
 // Find the comment with this ID
 // delete the comment with the ID of 823423
